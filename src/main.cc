0f55a6927bca19d18c4125210402c443ce970d80
arnau.ruana.i <arnau.ruana.i@est.fib.upc.edu>
2019-05-10 15:57:10 +0200

    piro a robòtica

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,9 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#include <set>
+using std::set;
+
 #include <vector>
 using std::vector;
 
@@ -17,29 +20,22 @@ using std::make_pair;
 
 typedef vector<int> Row;
 typedef vector<Row> Graph;
-typedef pair<pair<Graph, bool>, pair<Graph, bool> > Percolation;
+typedef pair<Graph, Graph> Percolation;
 
 // ========================================================================== //
 
 Percolation percolate(const Graph& graph, float q) {
-
-  // Graph nPerc = nodePercolation();
-  // Graph ePerc = edgePercolation();
-  return make_pair(make_pair(graph, false), make_pair(graph, true));
+  // Graph gPv = vertPercolation();
+  // Graph gPe = edgePercolation();
 }
 
 // -------------------------------------------------------------------------- //
 
 int main() {
-  Graph graph = readGraph();
-  cout << "Probabilitat de fallida: ";
-  float q; cin >> q;
-  Percolation perc = percolate(graph, q);
-  cout << endl << "Matriu adjacències graf percolat per nodes:" << endl;
-  printGraph(perc.first.first);
-  perc.first.second ? cout << "Percola" : cout << "No percola";
-  cout << endl << endl << "Matriu adjacències graf percolat per arestes:" << endl;
-  printGraph(perc.second.first);
-  perc.second.second ? cout << "Percola" : cout << "No percola";
-  cout << endl;
+  int nVert; cin >> nVert;
+  Graph graph(nVert);
+  graph.read();
+  for (int q = 0; q <= 1; q += 0.1) {
+    
+  }
 }
