145ae89e9e1ab72b4b17f694308c4834cdbf5ee9
arnau.ruana.i <arnau.ruana.i@est.fib.upc.edu>
2019-05-11 11:04:14 +0200

    DEBUGS trets

--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,7 @@
 #include <cmath>
 #include "graph.hh"
 using namespace std;
+
 // ========================================================================== //
 
 Graph percolateVert(const Graph& graph, float q)  {
@@ -29,13 +30,9 @@ double calcula_temps(double inici, double fi){
 // -------------------------------------------------------------------------- //
 
 int main() {
-	cout << "nVert" << endl; // DEBUG
   int nVert; cin >> nVert;	//nombre total de vertex del graf
   Graph graph(nVert);
-	cout << "GREAPH READ" << endl; // DEBUG
   graph.read();
-	graph.print(); // DEBUG
-	cout << "ALGO" << endl; // DEBUG
   int nombre_intervals = 20;													//nombre de punts en l'eix X que volem en el grafic (sense incloure q = 1)
   double increment = 1.0 / double(nombre_intervals);
   int nombre_experiments_per_q = 15;									//quants experiments realitzem per una determinada q
@@ -50,20 +47,14 @@ int main() {
   clock_t inici;
   for (double q = 0; q < 1; q += increment) {
     for (int i = 0; i < nombre_experiments_per_q; ++i){
-			cout << "percolateVert" << endl; // DEBUG
     	Graph graf_aux = percolateVert(graph, q);
-			cout << "percolateVert done" << endl; // DEBUG
     	//mesurem temps de trobar connexio en graf percolat per VERTEX
     	inici = clock();
-			cout << "connectagraekasdkd" << endl; // DEBUG
     	bool esConnex = connectaGraella (graf_aux, int(sqrt(nVert)) );
-			cout << "sldvkn done" << endl; // DEBUG
     	if (esConnex)	propietat_vert[id_interval] += 1.0;
     	temps_vert[id_interval] += calcula_temps(inici, clock());
     	//********************************
-			cout << "percolateEdge " << endl; //DEBUG
     	graf_aux = percolateEdge(graph, q);
-			cout << "percolateEdge done" << endl; // DEBUG
 			//mesurem temps de trobar connexio em graf percolat per EDGES
     	inici = clock();
     	esConnex = connectaGraella (graf_aux, int(sqrt(nVert)) );
